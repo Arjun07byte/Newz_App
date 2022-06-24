ecd707adac980095d18fb0d14ff2556be13015aa
Arjun Yadav <arjunyadavcs20@pgdav.du.ac.in>
2022-06-24 09:54:13 +0530

    Added Navigation

--- app/src/main/java/com/example/newzapp/uiComponents/NewsActivity.kt
+++ app/src/main/java/com/example/newzapp/uiComponents/NewsActivity.kt
@@ -2,12 +2,23 @@ package com.example.newzapp.uiComponents
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.newzapp.R
+import com.example.newzapp.databinding.ActivityNewsBinding
 
 class NewsActivity: AppCompatActivity() {
+    private lateinit var myViewBinding: ActivityNewsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_news)
-    }
+        myViewBinding = ActivityNewsBinding.inflate(layoutInflater)
+        setContentView(myViewBinding.root)
 
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        myViewBinding.bottomNavBar.setupWithNavController(navController)
+    }
 }
